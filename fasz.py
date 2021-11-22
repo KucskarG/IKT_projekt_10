777e1aa91bf7887087909a29f016287129b963af
KucskarG <90601194+KucskarG@users.noreply.github.com>
2021-11-22 10:34:18 +0100

    Add files via upload

---
+++ fasz.py
@@ -0,0 +1,43 @@
+#feladat11
+for x in range (0,11):
+    print (x)
+#feladat22
+for x1 in range (0,11,2):
+    print (x1)
+#feladat13
+for x2 in range (0,101,3):
+    print (x2)
+#feladat14
+i = int (input('Irj egy számot dilo '))
+print(i*2)
+#feladat15
+print("10")
+#feladat16
+for x3 in range (0,11):
+    print (x3*x3)
+
+#feladat17
+for x4 in range (0,31):
+    print (x4*x4)
+
+#feladat18
+for x5 in range (0,101,2):
+    print (x5)
+#feladat19
+i1 = int (input('Két számot irj be '))
+print(i1*i1)
+#feladat20
+for x6 in range (50,9,-2):
+    print (x6)
+#feladat21
+i2 = int (input('Ird be négyzet oldalát: '))
+print ('K ',i2*i2)
+print ('T', 4*i2)
+
+#feladat22
+i3 = int (input('Ird be téglalap oldalát: '))
+print ('K ',i3*i3)
+print ('T', 4*i3)
+
+
+

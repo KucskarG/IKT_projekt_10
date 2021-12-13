e443543b69ae8114194dec75fe100052cd53878b
KucskarG <90601194+KucskarG@users.noreply.github.com>
2021-12-13 10:32:41 +0100

    Add files via upload

---
+++ phada2.py
@@ -0,0 +1,23 @@
+def gyertyak (x):
+    if x==1:
+        print(r"""
+        I
+        """)
+    elif x==2:
+        print(r"""
+        II
+        """)
+    elif x==3:
+        print(r"""
+        III
+        """)
+    elif x==4:
+        print(r"""
+        IIII
+        """)
+
+
+nap = int (input("December hanyadika van: "))
+print (24-nap , " nap van még karácsonyig")
+if nap>5
+
